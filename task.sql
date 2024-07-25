b92ce53534643ddf0d8acc6c84d94cc5f0413111
vshvanska <valeriiashvanska@gmail.com>
2024-07-25 17:17:27 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,5 +1,4 @@
 -- Create database and tables
-
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
@@ -9,16 +8,32 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ProductInventory (
+CREATE TABLE Warehouses (
     ID INT,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
+    Name VARCHAR(50),
+    Address VARCHAR(50),
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
+
+CREATE TABLE Products (
+    ID INT,
+    Name VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE ProductInventory (
+    ID INT,
+    ProductID INT,
+    WarehouseAmount INT,
+    WarehouseID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+
 
 -- Populate test data
 
@@ -26,8 +41,16 @@ INSERT INTO Countries (ID,Name)
 	VALUES (1, 'Country1');
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
+
+INSERT INTO Warehouses (ID, Name, Address, CountryID)
+    VALUES (1, 'Warehouse-1', 'Street-1', 1);
+INSERT INTO Warehouses (ID, Name, Address, CountryID)
+    VALUES (2, 'Warehouse-2', 'Street-2', 2);
+
+INSERT INTO Products (ID, Name)
+VALUES (1, 'AwersomeProduct');
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ID,ProductID,WarehouseAmount,WarehouseID)
+	VALUES (1, 1, 2, 1);
+INSERT INTO ProductInventory (ID,ProductID,WarehouseAmount,WarehouseID)
+	VALUES (2, 1, 5, 1);
